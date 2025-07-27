1974876505368847bd79534190ebeb2da2202b73
Atul <dr6217811@gmail.com>
2025-07-27 13:33:25 +0530

    Optimal solution

--- majorityElement.js
+++ majorityElement.js
@@ -1,31 +1,24 @@
-/**
- * @param {number[]} nums
- * @return {number}
- */
-var majorityElement = function(nums) {
+var majorityElement = function (nums) {
+    if (nums.length < 2) {
+        return nums;
+    }
+    let map = new Map();
+    res = [];
+    for (value of nums) {
+        if (map.has(value)) {
+            map.set(value, map.get(value) + 1);
+        }
+        else {
+            map.set(value, 1);
+        }
+    }
+    for ([key, value] of map) {
+        if (value > nums.length / 3) {
+            res.push(key);
+        }
+    }
+    console.log(res)
+    return res;
 
-    let pivot = -1;
-    let count = 0;
-    let pivotCount = 0;
-    for(value of nums){
-        if(count === 0){
-            pivot = value;
-            count = 1;
-        }
-        else if(value != pivot){
-            count--;
-        }
-        else{
-            count++;
-        }
-    }
-    for(value of nums){
-        if(value === pivot){
-            pivotCount++;
-        }
-    }
-    if(pivotCount > nums.length / 2){
-        return pivot;
-    }
-    
 };
+majorityElement([2, 2]);
