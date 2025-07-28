1ef9a4d9b821bf134068c3e76913ba99b6ab1588
Atul <dr6217811@gmail.com>
2025-07-28 22:36:40 +0530

    New problem

---
+++ arrayConcatenation.js
@@ -0,0 +1,3 @@
+const getNewArray = (arr) => {
+    return [...arr, ...arr];
+}
--- stockBuySell.js
+++ stockBuySell.js
@@ -1,13 +1,39 @@
-function getProfit(prices){
-    let profit = 0
-    // 1 2 3 4 5
-    for(let i = 0; i < prices.length - 1; i++){
-        if(prices[i] < prices[i + 1]){
-            profit += prices[i + 1] - prices[i];
+/**
+ * @param {number[]} prices
+ * @return {number}
+ */
+var maxProfit = function(prices) {
+        // code here
+        let i = 0;
+        // 5 4 3 2 1
+        let j = 1;
+        let buy = -1;
+        let sell = -1;
+        let profit = 0;
+        while(j < prices.length - 1){
+            if(prices[i] < prices[j] && buy === -1){
+                    buy = i;
+                    j++;
+            }
+            if(prices[j] > prices[j+1]){
+                if(buy != -1){
+                    sell = j;
+                    profit += prices[sell] - prices[buy];
+                    buy = -1;
+                    i = j + 1;
+                    j = j + 1;
+                }
+            }
+            else {
+                j++;
+                i++;
+            }
         }
+        if(buy != -1){
+            profit += prices[prices.length - 1] - prices[buy]; 
+        }
+        console.log(profit);
     }
-    console.log(profit);
-}
 
-
-getProfit([1, 2, 3, 4, 5])
+    let arr = [7,6,4,3,1];
+    maxProfit(arr);
