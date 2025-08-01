6faadf29e613be9b1985e0047858b714a5c24b2f
Atul <dr6217811@gmail.com>
2025-08-01 15:41:16 +0530

    correct solution

--- week2/maxCircularSubarraySum.js
+++ week2/maxCircularSubarraySum.js
@@ -1,47 +1,24 @@
-/**
- * @param {number[]} nums
- * @return {number}
- */
-var maxSubarraySumCircular = function(arr) {
-    
-        let n = arr.length;
-        if(n === 1){
-            console.log(arr[0]);
-        }
-        
-        let total = 0;
-        
-        for(let i of arr){
-            total += i;
-        }
-        
-        let max = total;
-        let i = 0;
-        // [10, -3, -4, 7, 6, 5, -4, -1]
-        while(i < n){
-            let currMax = total;
-            let j;
-            if(i === 0 ) j = n -1;
-            else j = i - 1;
-            
-            while(j != i){
-                if(arr[j] < 0){
-                    if(i === 3){
-                        console.log(currMax, arr[j], j)
-                    }
-                    currMax = currMax - arr[j];
-                    j--;
-                    if(j < 0){
-                        j = n -1;
-                    }
-                }
-                else break;
-            }
-            currMax = Math.max(arr[i], currMax);
-            max = Math.max(currMax, max);
-             i++;
-        }
-       console.log(max);
-};
 
-maxSubarraySumCircular([1])
+        // code here
+        let minSum = arr[0];
+        let maxSum = arr[0];
+        
+        let curMin = arr[0];
+        let curMax = arr[0];
+        
+        let total = arr[0];
+        
+        for(let i = 1; i < arr.length; i++){
+            let val = arr[i];
+            total += val;
+            
+            curMax = Math.max(val, curMax + val);
+            maxSum = Math.max(maxSum, curMax);
+            
+            curMin = Math.min(val, curMin + val);
+            minSum = Math.min(minSum, curMin);
+        }
+      
+      if(maxSum < 0) return maxSum;
+      
+      return Math.max(maxSum, total - minSum);
