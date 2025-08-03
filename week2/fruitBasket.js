d5782a468084c0b0b84a8251694cf0fef28d90af
Atul <dr6217811@gmail.com>
2025-08-03 18:50:51 +0530

    factorail not using recursion

--- week2/fruitBasket.js
+++ week2/fruitBasket.js
@@ -2,10 +2,21 @@
 
 
 
-function fact(num) {
-    if(num === 1 || num === 0) return 1;
+// 7 * 6 * 5 * 4 * 3 * 2 * 1
 
-    return num * fact(num - 1);
+
+function fact(num) {
+
+    let result = 1;
+
+    if(num === 0 || num === 1) return 1;
+
+    for(let i = num; i > 0; i--){
+        result = result * i;
+    }
+
+    return result;
+    
 }
 
 
