d52a8b245e5327d656fe71fc1d33ab6e04fc2e61
Atul <dr6217811@gmail.com>
2025-09-15 16:39:29 +0530

    done

--- week7/Minimum Depth from leaf node.js
+++ week7/Minimum Depth from leaf node.js
@@ -1,15 +1,4 @@
-/**
- * Definition for a binary tree node.
- * function TreeNode(val, left, right) {
- *     this.val = (val===undefined ? 0 : val)
- *     this.left = (left===undefined ? null : left)
- *     this.right = (right===undefined ? null : right)
- * }
- */
-/**
- * @param {TreeNode} root
- * @return {number}
- */
+
 function height(root) {
     if (!root) return 0;
     if(!root.left) {
@@ -29,4 +18,4 @@ var minDepth = function (root) {
     
 
     return height(root);
-};
+}; 
