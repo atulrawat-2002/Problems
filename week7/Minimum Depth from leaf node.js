1eb553686a58efc056d66be8f9a6071db7417292
Atul <dr6217811@gmail.com>
2025-09-15 17:07:16 +0530

    Minimum depth using BFS

--- week7/Minimum Depth from leaf node.js
+++ week7/Minimum Depth from leaf node.js
@@ -1,4 +1,4 @@
-
+/********************************* USING DFS ********************** */
 function height(root) {
     if (!root) return 0;
     if(!root.left) {
@@ -19,3 +19,43 @@ var minDepth = function (root) {
 
     return height(root);
 }; 
+
+/*************************************** USING BFS ****************************************** */
+
+
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number}
+ */
+
+var minDepth = function(root) {
+    let levels = 0;
+    let q = [];
+    if(!root) return 0;
+    q.push(root);
+
+    while(q.length) {
+        levels++;
+        let levelSize = q.length;
+        while(levelSize) {
+            let data = q.shift();
+
+            if(!data.left && !data.right) return levels;
+
+            if(data.left) q.push(data.left);
+            if(data.right) q.push(data.right);
+
+            levelSize--;
+        }
+    }
+
+    return levels;
+};
