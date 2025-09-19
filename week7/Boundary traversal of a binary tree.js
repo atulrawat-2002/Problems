2e7d898b3e368aa9c2f0968be8ae1da055245003
Atul <dr6217811@gmail.com>
2025-09-19 11:12:29 +0530

    Boundary traversal using morris traversal

--- week7/Boundary traversal of a binary tree.js
+++ week7/Boundary traversal of a binary tree.js
@@ -50,12 +50,96 @@ class Solution {
     }
 }
 
+/****************************** Boundary traversal using MORRIS TRAVERSAL ***************************** */
+
+
+
+class Solution {
+    boundaryTraversal(root) {
+        // code here
+        function isLeaf(root) {
+            return !root.left && !root.right;
+        }
+        
+        function getPred(node) {
+            let temp = node.left;
+            while(temp.right && temp.right != node) {
+                temp = temp.right;
+            }
+            return temp;
+        }
+        
+        function lefts(node) {
+            if(node === null) return;
+            
+            let temp = node;
+            
+            while(!isLeaf(temp)) {
+                ans.push(temp.data);
+                
+                if(temp.left) temp = temp.left;
+                else if(temp.right) temp = temp.right;
+            }
+        }
+        
+        function rights(node) {
+            if(node === null) return;
+            let temp = node;
+            let cur = [];
+            
+            while(!isLeaf(temp)) {
+                cur.push(temp.data);
+                
+                if(temp.right) temp = temp.right;
+                else if(temp.left) temp = temp.left;
+            }
+            
+            ans.push(...cur.reverse());
+        }
+        
+        function leaves(node) {
+            let cur = node;
+            
+            while(cur) {
+                if(cur.left === null) {
+                    if(cur.right === null)
+                        ans.push(cur.data);
+                        
+                    cur = cur.right;
+                } else {
+                    let pred = getPred(cur);
+                    
+                    if(pred.right === null) {
+                        pred.right = cur;
+                        cur = cur.left;
+                    } else {
+                        if(pred.left === null) 
+                            ans.push(pred.data);
+                            
+                        pred.right = null;
+                        cur = cur.right;
+                    }
+                }
+            }
+        }
+        
+        let ans = [];
+        
+        if(!isLeaf(root)) ans.push(root.data);
+        
+        lefts(root.left);
+        
+        leaves(root);
+        
+        rights(root.right);
+        
+        return ans;
+        
+    }
+}
 
 
 
 
 
-
-
-
-
+ 
