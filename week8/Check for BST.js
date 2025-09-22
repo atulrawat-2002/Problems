b81e25206c8e41eeac0f4bb331b3b715298cdf16
Atul <dr6217811@gmail.com>
2025-09-22 10:23:10 +0530

    checking BST using inorder traversal and constant space

--- week8/Check for BST.js
+++ week8/Check for BST.js
@@ -19,3 +19,40 @@ var isValidBST = function(root) {
 
 
 /******************************** USING MIN AND MAX RANGES *********************** */
+
+function check(root, min, max ) {
+    if(root === null) return true;
+
+    if(root.val < min || root.val > max) return false;
+
+    return check(root.left, min, root.val - 1) && 
+           check(root.right, root.val + 1, max )
+}
+var isValidBST = function(root) {
+
+    return check(root, -Infinity, Infinity);
+};
+
+
+/******************************** CHECKING BST USING INORDER TRAVERSAL AND O(1) SPACE********************** */
+
+
+class Solution {
+    // Function to check whether a Binary Tree is BST or not.
+    isBST(root) {
+        // your code here
+        function inorder (root) {
+            if(root === null) return true;
+            
+            if(!inorder(root.left)) return false;
+            
+            if(prev >= root.data) return false;
+            
+            prev = root.data;
+            
+            return inorder(root.right);
+        }
+        let prev = -Infinity;
+        return inorder(root);
+    }
+}
