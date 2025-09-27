0f55a8c12bfa2aa57dbd901e2ba664b161460b82
Atul <dr6217811@gmail.com>
2025-09-27 06:29:33 +0530

    Bubble sort Unoptimized

---
+++ week9/bubble Sort.js
@@ -0,0 +1,28 @@
+let arr = [5, 3, 4, 0, 2, 1, 6, 8, 7];
+
+
+function bubbleSort() {
+    for(let i = 0; i < arr.length; i++) {
+        for(let j = i + 1; j < arr.length; j++) {
+            console.log("i is ", i, " and j is ", j)
+            console.log(`Comparing ${arr[i]} with ${arr[j]}`)
+            if(arr[i] > arr[j]) {
+                console.log(`swapping ${arr[i]} is greater then ${arr[j]}`)
+                swap(i, j)
+                console.log(arr);
+                
+            }
+        }
+    }
+}
+
+function swap(i, j) {
+    [arr[j], arr[i]] = [arr[i], arr[j]]
+}
+
+
+
+console.log(arr);
+bubbleSort();
+
+// console.log(arr);
