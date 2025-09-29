e2da78bc449d7a810ae09d04b97d1ee66f9e2893
Atul <dr6217811@gmail.com>
2025-09-29 09:40:38 +0530

    quick sort notes

--- Alogorithms/quickSort.js
+++ Alogorithms/quickSort.js
@@ -1,25 +1,96 @@
-let arr = Array(100000).fill(7);
+// let arr = Array(5).fill(4);
+let arr = [4, 3, 5, 1, 0, 2, -3, 8, 9, -2];
 
 function swap(a, b) {
   [arr[a], arr[b]] = [arr[b], arr[a]];
 }
 
 function partition(low, upp) {
-  let start = low, end = upp;
-  let pivot = arr[low];
-  while(start < end) {
-    while(start <= upp && arr[start] < pivot) start++; 
-    while(end >= low && arr[end] > pivot) end--;
+  let i = low;
+  let j = upp;
+  let p = low;
 
-    if(start < end) swap(start, end);
+  while (i < j) {
+    do i++;
+    while (arr[i] <= arr[p]);
+    do j--;
+    while (arr[j] > arr[p]);
+
+    if (i < j) swap(i, j);
   }
-  swap(low, end);
-
-  return end;
-
+    swap(p, j);
+    return j;
+    
 }
 
 function quickSort(low, upp) {
+  if (low < upp) {
+    let pos = partition(low, upp);
+    quickSort(low, pos);
+    quickSort(pos + 1, upp);
+  }
+}
+
+console.log(arr);
+arr.push(Infinity);
+quickSort(0, arr.length - 1);
+console.log(arr);
+
+
+/*******************************  NOTES  ****************** */
+
+/*
+
+If we use Infinity or the maximum value at the end of the array, we can prevent the array from going to end.
+if use this version of algorithm 
+let i = low;
+  let j = upp;
+  let p = low;
+
+  while (i < j) {
+    do i++;
+    while (arr[i] <= arr[p]);
+    do j--;
+    while (arr[j] > arr[p]);
+
+    if (i < j) swap(i, j);
+  }
+    swap(p, j);
+    return j;
+ 
+
+    and we need to exclude the positio returned form the partition function while calling the recursion like this 
+
+    function quickSort(low, upp) {
+  if (low < upp) {
+    let pos = partition(low, upp);
+    quickSort(low, pos);
+    quickSort(pos + 1, upp);
+  }
+}
+
+
+
+AND IF DO NOT USE INFINITY AND WRITE THE ALGORITHM IN DEFFERENT MANNER IT WILL WORK FINE FOR OTHER CASES BUT WIL GOT INTO THE 
+INFINITE RECURSION BECAUSE WE WILL CALL THE SAME RECURSION AGAIN AND AGIAN, LIKE THIS
+
+ while (i < j) {
+    while(i <= high && a[i] <= p);
+    while(j >= low && a[j] > p);
+    
+    if(i < j) swap(a[i], a[j]);
+  }
+    
+
+  swap(p, a[j]);
+  return j;
+
+
+
+  AND WHILE CALLING THE RECURSION WE HAVE TO INCLUDE THE VALUE OF RETURNED INDEX AGAIN, LIKE THIS
+
+
+       function quickSort(low, upp) {
   if (low < upp) {
     let pos = partition(low, upp);
     quickSort(low, pos - 1);
@@ -27,8 +98,8 @@ function quickSort(low, upp) {
   }
 }
 
+ IN THIS CASE AND WE CAN ALSO CHOOSE THE STARTING ELEMENT AS THE PIVOT AS WELL 
 
 
-console.log(arr);
-quickSort(0, arr.length - 1);
-console.log(arr)
+
+    */
