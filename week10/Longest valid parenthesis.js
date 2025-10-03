d1c06bd4af9213bc2393fc036355e841b5c78f83
Atul <dr6217811@gmail.com>
2025-10-03 10:27:24 +0530

    Longest valid parenthesis Using Opening and Closing count and two traversal

--- week10/Longest valid parenthesis.js
+++ week10/Longest valid parenthesis.js
@@ -21,3 +21,36 @@ class Solution {
         return maxLen;
     }
 }
+
+/*************************** USING TWO TRAVERSALS AND OPENING AND CLOSING COUNT ******************************** */
+
+
+class Solution {
+    maxLength(s) {
+        // code here
+        let op = 0, cl = 0, maxRight = 0, maxLeft = 0;
+        
+        for(let i = 0; i < s.length; i++) {
+            if(s[i] === '(') op++;
+            else cl++;
+            
+            if(op === cl) maxRight = Math.max(maxRight, op * 2);
+            else if(cl > op) op = 0, cl = 0;
+        }
+        
+        op = 0, cl = 0;
+        
+        for(let i = s.length - 1; i >= 0; i--) {
+            if(s[i] === '(') op++;
+            else cl++;
+            
+            if(op === cl) maxLeft = Math.max(maxLeft, op * 2);
+            else if(op > cl) op = 0, cl = 0;
+        }
+        
+        return Math.max(maxLeft, maxRight);
+    }
+}
+
+
+
