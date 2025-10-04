e073ea2caa24ed0a805f3d25119becdade592a81
Atul <dr6217811@gmail.com>
2025-10-04 17:58:14 +0530

    longest valid parenthesis using dynamic programming

--- week10/Longest valid parenthesis.js
+++ week10/Longest valid parenthesis.js
@@ -53,4 +53,22 @@ class Solution {
 }
 
 
+/************************* USING DYNAMIC PROGRAMMING *************** */
 
+var longestValidParentheses = function(s) {
+    let dp = new Array(s.length).fill(0);
+    let maxLen = 0;
+
+    for (let i = 1; i < s.length; i++) {
+        if (s[i] === ')') {
+            if (s[i - 1] === '(') {
+                dp[i] = (i >= 2 ? dp[i - 2] : 0) + 2;
+            } else if (i - dp[i - 1] - 1 >= 0 && s[i - dp[i - 1] - 1] === '(') {
+                dp[i] = dp[i - 1] + ((i - dp[i - 1] - 2) >= 0 ? dp[i - dp[i - 1] - 2] : 0) + 2;
+            }
+            maxLen = Math.max(maxLen, dp[i]);
+        }
+    }
+
+    return maxLen;
+};
