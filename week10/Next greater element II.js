1b207b7117fe9f527113812f0f58331ac586dd4b
Atul <dr6217811@gmail.com>
2025-10-06 05:48:23 +0530

    Next greater element II two passes

--- week10/Next greater element II.js
+++ week10/Next greater element II.js
@@ -16,3 +16,22 @@ var nextGreaterElements = function(nums) {
 
     return ans;
 };
+
+/************************ TWO PASSES ***********************/
+
+var nextGreaterElements = function(nums) {
+    let n = nums.length;
+    let ans = new Array(n).fill(-1);
+    let stack = [];
+
+    for (let i = 0; i < 2 * n; i++) {
+        let cur = nums[i % n];
+        while (stack.length && nums[stack[stack.length - 1]] < cur) {
+            let top = stack.pop();
+            ans[top] = cur;
+        }
+        if (i < n) stack.push(i);
+    }
+
+    return ans;
+};
