728dad9742d4dc657370f85eac32c15cbaf4d535
Atul <dr6217811@gmail.com>
2025-10-08 07:44:12 +0530

    By calculating left smaller on the fly

--- week10/Largest rectangle in histogram.js
+++ week10/Largest rectangle in histogram.js
@@ -43,6 +43,49 @@ class Solution {
 }
 
 
+/********************** CALCULATING LEFT SMALLER ON THE FLY ****************/
+
+
+class Solution {
+    getMaxArea(arr) {
+        // code here
+        let n = arr.length;
+        let stack = [];
+        let right = new Array(n).fill(n);
+        
+        
+        for(let i = n - 1; i >= 0; i--) {
+            while(stack.length && arr[stack[stack.length - 1]] >= arr[i]) {
+                stack.pop();
+            }
+            if(stack.length) {
+                right[i] = stack[stack.length - 1];
+            }
+            stack.push(i);
+        }
+        
+        while(stack.length) stack.pop();
+        let maxArea = 0;
+        
+        for(let i = 0; i < n; i++) {
+            let area = 0;
+            while(stack.length && arr[stack[stack.length - 1]] >= arr[i]) {
+                stack.pop();
+            }
+            if(stack.length) {
+                area = arr[i] * ( right[i] - stack[stack.length - 1] - 1 );
+            } else {
+                area = arr[i] * ( right[i] - (-1) - 1 );
+            }
+            stack.push(i);
+            maxArea = Math.max(area, maxArea);
+        }
+        
+        return maxArea;
+    }
+}
+
+
 
 
 
