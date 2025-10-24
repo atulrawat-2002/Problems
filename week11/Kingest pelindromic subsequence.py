cacd78cb859dbab072285344e14d2aa7b3d17e35
Atul <dr6217811@gmail.com>
2025-10-24 11:10:37 +0530

    using n square space

--- week11/Kingest pelindromic subsequence.py
+++ week11/Kingest pelindromic subsequence.py
@@ -22,4 +22,23 @@ class Solution:
         return cur[n - 1]
                     
                 
-      
+#  USING O(n2) SPACE
+
+class Solution:
+    def longestPalindromeSubseq(self, s: str) -> int:
+
+        n = len(s)
+
+        dp = [[0] * n for _ in range(n)]
+
+        for i in range(n - 1, -1, -1):
+            for j in range(i, n - 1):
+                if i == j:
+                    dp[i][j] = 1
+                elif s[i] == s[j]:
+                    dp[i][j] = 2 + dp[i + 1][j - 1]
+                else:
+                    dp[i][j] = max( dp[i + 1][j], dp[i][j - 1] )
+
+        return dp[0][n - 1]
+        
